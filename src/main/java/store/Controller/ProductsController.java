c778ec6a55c3de43a23b1ceef133178d14f8994f
박근채 <pgc0419@gmail.com>
2024-11-07 12:30:00 +0900

    feat: 상품명 리스트와 갯수 리스트로 변환

--- src/main/java/store/Controller/ProductsController.java
+++ src/main/java/store/Controller/ProductsController.java
@@ -6,14 +6,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductsController {
-    public List<String> parseProduct() {
-        String input = InputView.readProduct();
-        String[] productArray = input.split(",");
-        List<String> productList = new ArrayList<>();
+    public List<String> parseNames() {
+        List<String> names = new ArrayList<>();
+        String[] productArray = InputView.readProduct();
+
         for (String product : productArray) {
             product = product.trim().replace("[", "").replace("]", "");
-            productList.add(product);
+            String[] parts = product.split("-");
+            names.add(parts[0].trim());
         }
-        return productList;
+
+        return names;
+    }
+
+    public List<String> parseQuantities() {
+        List<String> quantities = new ArrayList<>();
+        String[] productArray = InputView.readProduct();
+
+        for (String product : productArray) {
+            product = product.trim().replace("[", "").replace("]", "");
+            String[] parts = product.split("-");
+            quantities.add(parts[1].trim());
+        }
+
+        return quantities;
     }
 }
--- src/main/java/store/View/InputView.java
+++ src/main/java/store/View/InputView.java
@@ -3,9 +3,10 @@ package store.View;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static String readProduct() {
+    public static String[] readProduct() {
         System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
-        return Console.readLine().trim();
+        String input = Console.readLine();
+        return input.split(",");
     }
 
     public String promotion() {
